d85695186e1a468eb4a9cd7db211f69016705715
PedroBarata <pedrofelipebarata@gmail.com>
2020-11-30 22:14:45 -0300

    fix: validação no telefone

--- src/main/java/escola/Telefone.java
+++ src/main/java/escola/Telefone.java
@@ -5,8 +5,14 @@ public class Telefone {
     private String numero;
 
     public Telefone(String ddd, String numero) {
-        if (ddd == null || numero == null || !ddd.matches("\\d{2}/") || !numero.matches("\\d{8,9}/")) {
-            throw new IllegalArgumentException("DDD ou Numero inválidos!");
+        if (ddd == null || numero == null) {
+            throw new IllegalArgumentException("DDD e Numero são obrigatórios!");
+        }
+        if(!ddd.matches("\\d{2}")) {
+            throw new IllegalArgumentException("DDD inválido!");
+        }
+        if (!numero.matches("\\d{8}|\\d{9}")) {
+            throw new IllegalArgumentException("Número inválido!");
         }
         this.ddd = ddd;
         this.numero = numero;
